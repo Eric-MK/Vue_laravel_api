d5e3209af53ce19f3d9b341374d75ab3ae620c1e
unknown <mutungaeric839@gmail.com>
2023-05-03 05:47:07 +0300

    model skill using protected

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Skill extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name','slug'];//the $fillable property is set to ['name','slug'], which means that these two attributes can be set en masse using the create() or update() methods.
 }
