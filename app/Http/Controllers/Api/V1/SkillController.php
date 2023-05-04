1548f1e5fd7e66c53b6bed129c83e160f75023b9
unknown <mutungaeric839@gmail.com>
2023-05-04 20:21:21 +0300

    created StoreSkilledRequest will contain rules that define how to validate the data submitted in a request when creating or updating a Skill model

--- app/Http/Controllers/Api/V1/SkillController.php
+++ app/Http/Controllers/Api/V1/SkillController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreSkillRequest;
 use Illuminate\Http\Request;
 
 class SkillController extends Controller
 {
-    //
+    public function index()
+    {
+
+        return response()->json("skill index");
+
+    }
+
+    public function store(StoreSkillRequest $req)
+    {
+
+    }
 }
