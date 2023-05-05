ae073d56a08d0f9ea2b67387ec9965e61ebffb7f
unknown <mutungaeric839@gmail.com>
2023-05-05 12:09:09 +0300

    another way to ensure during updating the slug attribute can remain the same

--- app/Http/Requests/StoreSkillRequest.php
+++ app/Http/Requests/StoreSkillRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreSkillRequest extends FormRequest
@@ -23,9 +24,12 @@ class StoreSkillRequest extends FormRequest
     {
         return [
             'name' => ['required','min:3','max:20'],
-            'slug' => ['required','unique:skills,slug,' . $this->skill->id]
+           // 'slug' => ['required','unique:skills,slug,' . $this->skill->id]
+            'slug' => ['required',Rule::unique('skills')->ignore($this->skill)]//another way that works
         ];
     }
+/*  Rule::unique('skills')->ignore($this->skill)  In this case, skills is the name of the table where the slug field should be unique. The second argument to the method is an optional parameter that can be used to specify the name of the column to use for the unique check. If this argument is not provided, Laravel assumes that the name of the column is the same as the name of the field being validated.
+ */
    /* By including $this->skill->id in the validation rule, Laravel will exclude the current record being updated from the check for uniqueness. This ensures that the slug field remains unique among all other records in the skills table, but still allows the user to keep the same slug value when updating a record.
    is used in a validation rule to specify that the slug field must be unique among all the records in the skills table except for the current record being updated.*/
 /* $this refers to the instance of the Form Request class that is being used to validate the request. skill is a property on the Form Request class that holds the current record being updated. id is a property on the skill object that holds the ID of the current record.*/
