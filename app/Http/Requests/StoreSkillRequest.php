cf7d5d7e7ab91555b179eceab59e9d632d317538
unknown <mutungaeric839@gmail.com>
2023-05-05 11:59:07 +0300

    making slug attribute to keep the same value when updating

--- app/Http/Requests/StoreSkillRequest.php
+++ app/Http/Requests/StoreSkillRequest.php
@@ -23,7 +23,11 @@ class StoreSkillRequest extends FormRequest
     {
         return [
             'name' => ['required','min:3','max:20'],
-            'slug' => ['required','unique:skills,slug']//The slug field is required and must be unique in the skills table, except for the current record
+            'slug' => ['required','unique:skills,slug,' . $this->skill->id]
         ];
     }
+   /* By including $this->skill->id in the validation rule, Laravel will exclude the current record being updated from the check for uniqueness. This ensures that the slug field remains unique among all other records in the skills table, but still allows the user to keep the same slug value when updating a record.
+   is used in a validation rule to specify that the slug field must be unique among all the records in the skills table except for the current record being updated.*/
+/* $this refers to the instance of the Form Request class that is being used to validate the request. skill is a property on the Form Request class that holds the current record being updated. id is a property on the skill object that holds the ID of the current record.*/
+
 }
