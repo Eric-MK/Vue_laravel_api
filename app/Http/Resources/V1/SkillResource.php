b718552b493b06940333fcd670e923ef1eac8063
unknown <mutungaeric839@gmail.com>
2023-05-05 12:39:15 +0300

    comments for better understanding

--- app/Http/Resources/V1/SkillResource.php
+++ app/Http/Resources/V1/SkillResource.php
@@ -17,3 +17,8 @@ class SkillResource extends JsonResource
         return parent::toArray($request);
     }
 }
+
+/* A resource class in Laravel is responsible for transforming model data into a JSON representation that can be returned as a response from an API endpoint. The resource class defines how the model data should be presented in the response, including which fields to include, how to format the data, and any additional meta information that should be included.
+
+By convention, resource classes in Laravel are typically named after the model they represent and are stored in the app/Http/Resources directory. */
+
